d45514c545d6b6615a3e5395de3e6795b2c03b12
gigster99 <gig.graham@afdsi.org>
2014-12-01 11:05:42 -0600

    new_3

--- conf.py
+++ conf.py
@@ -31,9 +31,9 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '_exten
 # Add any Sphinx extension module names here, as strings.
 # They can be extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-              'sphinxcontrib.bibtex',
-              'sphinxcontrib.httpdomain',
-              ]
+#              'sphinxcontrib.bibtex',
+#              'sphinxcontrib.httpdomain',
+             ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
