bbe95fdbc08fda2b97be6958568fec3dc177c3e8
gigster99 <gig.graham@afdsi.org>
2014-12-02 08:26:43 -0600

    restored napoleon

--- conf.py
+++ conf.py
@@ -30,21 +30,21 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '_exten
 # They can be extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
               'sphinx.ext.autodoc',
-#              'sphinx.ext.napoleon',
+              'sphinxcontrib.napoleon',
               'sphinxcontrib.bibtex',
 #              'sphinxcontrib.httpdomain',
              ]
 # Napoleon settings
-#napoleon_google_docstring = True
-#napoleon_numpy_docstring = True
-#napoleon_include_private_with_doc = False
-#napoleon_include_special_with_doc = True
-#napoleon_use_admonition_for_examples = False
-#napoleon_use_admonition_for_notes = False
-#napoleon_use_admonition_for_references = False
-#napoleon_use_ivar = False
-#napoleon_use_param = True
-#napoleon_use_rtype = True
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
